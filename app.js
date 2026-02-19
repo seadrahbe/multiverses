5b85ee21267f53716f874ec75ff67143cbd6bc89
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-02-19 14:42:34 -0800

    Added route and port number

---
+++ app.js
@@ -0,0 +1,25 @@
+// Import the express module
+import express from 'express';
+
+// Create an express application
+const app = express();
+
+// Define a port number where server will listen
+const PORT = 3009;
+
+// Enable static file serving
+app.use(express.static('public'));
+
+// "Middleware" allows express to read form data and store it in req.body
+app.use(express.urlencoded({ extended: true }));
+
+// Define our main route ('/')
+// Default route
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+// Start server and listen on designated port
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
