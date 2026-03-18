cc4b4a402119c8ecfc7bdae55c18f503002b2973
onjamm <75151498+onjamm@users.noreply.github.com>
2026-03-17 20:45:44 -0700

    working on adding a modal to add another layer before deletion

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,11 @@
-/* MODAL LOGIC -s */
+/* POP UP POEM MODAL LOGIC -s */
 
 addEventListener("DOMContentLoaded", (event) => { 
   // List to store poems
   const poems = document.querySelectorAll(".poem");
 
   // Get modal from DOM
-  const modal = document.getElementById("poem_modal");
+  const modal = document.getElementById("poem-modal");
 
   // Function for opening the modal
   function openModal() {
@@ -30,4 +30,43 @@ addEventListener("DOMContentLoaded", (event) => {
   // Add close event listener to close button 
   close.addEventListener("click", closeModal);
 
+  /*DELETE MODAL LOGIC */
+
+  //Variable to store poem to delete
+  let poemToDelete = null; 
+
+  //Get delete modal from dom
+  const deleteModal = document.getElementById("delete-modal");
+  console.log("delete modal:", deleteModal);
+
+  //Function for opening delete modal
+  function openDeleteModal() {
+    deleteModal.style.display = "flex";
+  }
+
+  //Assign click listener to each delete button (trashcan)
+  document.querySelectorAll(".delete-btn").forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation(); 
+        poemToDelete = btn.closest("form");
+        openDeleteModal();
+
+    })
+  })
+
+  //Function for closing delete modal
+  function closeDeleteModal() {
+    deleteModal.style.display = "none";
+  }
+
+  //Close delete modal when cancel button is clicked
+  document.querySelector(".cancel-delete").addEventListener("click", closeDeleteModal);
+
+  //Submit delete form when confirm button is clicked
+  document.querySelector(".confirm-delete").addEventListener("click", () => {
+    if (poemToDelete) {
+        poemToDelete.submit();
+    }
+  });
 })
