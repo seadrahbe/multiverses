4116f1533e3376b015833ae717d289665c11c066
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-03-17 18:19:25 -0700

    More modal updates with open and close functionality, fixed small bug in head introduced in last commit

---
+++ public/js/home.js
@@ -0,0 +1,33 @@
+/* MODAL LOGIC -s */
+
+addEventListener("DOMContentLoaded", (event) => { 
+  // List to store poems
+  const poems = document.querySelectorAll(".poem");
+
+  // Get modal from DOM
+  modal = document.getElementById("poem_modal");
+
+  // Function for opening the modal
+  function openModal() {
+    modal.style.display = "flex";
+  }
+
+  // Function for closing the modal
+  function closeModal() {
+    modal.style.display = "none";
+  }
+
+  // Assign "click" event listener to each poem
+  poems.forEach(poem => 
+    poem.addEventListener("click", openModal)
+  );
+
+  console.log(poems);
+
+  // Get close button element
+  const close = document.getElementById("close-button");
+
+  // Add close event listener to close button 
+  close.addEventListener("click", closeModal);
+
+})
