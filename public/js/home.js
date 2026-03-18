c01c641e2e09c244c4123516b09af6e9de63cb68
onjamm <75151498+onjamm@users.noreply.github.com>
2026-03-17 21:20:18 -0700

    Finished implementing the modal for deleting a poem, and making it pretty!

--- public/js/home.js
+++ public/js/home.js
@@ -30,43 +30,4 @@ addEventListener("DOMContentLoaded", (event) => {
   // Add close event listener to close button 
   close.addEventListener("click", closeModal);
 
-  /*DELETE MODAL LOGIC */
-
-  //Variable to store poem to delete
-  let poemToDelete = null; 
-
-  //Get delete modal from dom
-  const deleteModal = document.getElementById("delete-modal");
-  console.log("delete modal:", deleteModal);
-
-  //Function for opening delete modal
-  function openDeleteModal() {
-    deleteModal.style.display = "flex";
-  }
-
-  //Assign click listener to each delete button (trashcan)
-  document.querySelectorAll(".delete-btn").forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        e.stopPropagation(); 
-        poemToDelete = btn.closest("form");
-        openDeleteModal();
-
-    })
-  })
-
-  //Function for closing delete modal
-  function closeDeleteModal() {
-    deleteModal.style.display = "none";
-  }
-
-  //Close delete modal when cancel button is clicked
-  document.querySelector(".cancel-delete").addEventListener("click", closeDeleteModal);
-
-  //Submit delete form when confirm button is clicked
-  document.querySelector(".confirm-delete").addEventListener("click", () => {
-    if (poemToDelete) {
-        poemToDelete.submit();
-    }
-  });
-})
+});
