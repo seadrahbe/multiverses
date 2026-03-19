93a4dc3e0bc128d2a46098de89796c67f7fda2bd
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-03-18 18:35:38 -0700

    More modal work, merging

--- public/js/home.js
+++ public/js/home.js
@@ -4,12 +4,30 @@ addEventListener("DOMContentLoaded", (event) => {
   // List to store poems
   const poems = document.querySelectorAll(".poem");
 
-  // Get modal from DOM
-  const modal = document.getElementById("poem_modal");
+  // Get modal elements from DOM
+  const modal = document.getElementById("poem-modal");
+  const modal_title = document.getElementById("modal-poem-title");
+  const modal_author = document.getElementById("modal-poem-author");
+  const modal_date = document.getElementById("modal-poem-date");
+  const modal_poem = document.getElementById("modal-poem-body");
 
-  // Function for opening the modal
-  function openModal() {
+  // Function for opening the modal & displaying info
+  function openModal(poem) {
     modal.style.display = "flex";
+
+    // Get relevant fields from poem element
+    const poem_title_html = poem.children[1].firstElementChild.innerHTML;
+    const poem_author_html = poem.children[1].children[1].innerHTML;
+    console.log(poem_author_html);
+    const poem_date_html = poem.children[1].children[2].innerHTML;
+
+    
+    // Set modal fields to poem element inner HTML
+    modal_title.innerHTML = poem_title_html;
+    modal_author.innerHTML = poem_author_html;
+    modal_date.innerHTML = poem_date_html;
+
+
   }
 
   // Function for closing the modal
@@ -21,8 +39,6 @@ addEventListener("DOMContentLoaded", (event) => {
   poems.forEach(poem => 
     poem.addEventListener("click", openModal)
   );
-
-  console.log(poems);
 
   // Get close button element
   const close = document.getElementById("close-button");
